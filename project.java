70e97e874bd079e91a67e9b1861f51701eed1b56
Yeah Jiah Xuan <b250035a@sc.edu.my>
2026-08-11 00:17:33 +0800

    1

--- project.java
+++ project.java
@@ -1,34 +1,3 @@
-//package application;   
-import javafx.application.Application;  
-import javafx.event.ActionEvent;  
-import javafx.event.EventHandler;  
-import javafx.scene.Scene;  
-import javafx.scene.control.Button;  
-import javafx.stage.Stage;  
-import javafx.scene.layout.StackPane;  
-
-public class project extends Application {  
-  
-    @Override  
-    public void start(Stage primaryStage) throws Exception { 
-        Button btn1 = new Button("Say, Hello World"); 
-        btn1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World");
-            }
-        });
-        
-        StackPane root = new StackPane();  
-        root.getChildren().add(btn1);
-        
-        Scene scene = new Scene(root, 600, 400);
-        primaryStage.setTitle("First JavaFX Application");  
-        primaryStage.setScene(scene);  
-        primaryStage.show();
-    }
-    public static void main (String[] args)  
-    {  
-        launch(args);  
-    } 
+public class project {  
+    
 }
